73d2a7ac62a0a7221196ddb715e5977ce1190177
ntnikhilesh <ntnikhilesh@gmail.com>
2018-01-21 14:50:05 +0530

    simple redux exp without react

--- src/index.js
+++ src/index.js
@@ -1,8 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+
+//Redux without React
+
+import {createStore} from 'redux';
+
+//create reducer that accept action dispatched by componente
+
+const reducer=(state,action)=>{
+    //one reducer can handle multiple actions
+    switch (action.type){
+        case 'ADD':
+            state=state+action.payload;
+            break;
+        case 'SUBTRACT':
+            state=state-action.payload;
+            break;
+    }
+    //reducer must return state
+    return state;
+}
+//create store that holds states(initialize with first state)
+const store= createStore(reducer,1)
+
+//this fuction get executed once store updated
+store.subscribe(()=>{
+    console.log('Store updated!',store.getState());
+});
+
+//dispatch the action
+store.dispatch({
+    type:'ADD',
+    payload:10 //payload of the action
+})
+
+store.dispatch({
+    type:'ADD',
+    payload:100 //payload of the action
+})
+
+store.dispatch({
+    type:'SUBTRACT',
+    payload:100 //payload of the action
+})
